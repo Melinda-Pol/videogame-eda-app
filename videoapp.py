73aa0f4a040268997fe0e673d2dabab547ec7ea0
melinda-pol antonio dazo <melinda_2212@hotmail.com>
2020-11-23 11:57:38 +0100

    dep2

--- videoapp.py
+++ videoapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import matplotlib
+#import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
